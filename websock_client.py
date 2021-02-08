8d3b97a2cf355391973b7cfc72c997cc51f0b48e
Adam <adam.funzone@gmail.com>
2021-02-08 16:56:04 +0100

    h

--- websock_client.py
+++ websock_client.py
@@ -4,36 +4,61 @@ import json
 import time
 from datetime import datetime
 
+FILE_NAME = input("please enter the name of the json file name<>.jsonc </>")
+DUMMY_FILE_NAME = "dummy.jsonc"
+
 
 def open_as_json(path):
     with open(path,"r") as fin:
         json_data = json.loads(fin.read())
-        return json_data["packets"],json_data["time"]
+        return json_data["packets"],json_data["time"],json_data["satellite"],json_data["type"]
 
 
+def write_response_tofile(packet,response):
+    with open("w", "response.txt") as fout:
+        fout.write(packet + ":" + response)
 
-async def send_packets(uri):
-    async with websockets.connect(uri) as websocket:
-        json_data,time_to_send = open_as_json("playlist_4.jsonc")
-        while True:
-            if datetime.now().strftime("%d/%m/%Y %H:%M:%S") == time_to_send:
+]
+def reset_json():
+    dummy_file_data = ""
+    with open("r", DUMMY_FILE_NAME) as fin:
+        dummy_file_data = fin.read()
+
+    with open("w", FILE_NAME) as fout:
+        fout.write(dummy_file_data)
+
+
+def send_packets():
+    json_data,time_to_send,satellite,telecomand = open_as_json(FILE_NAME)
+    while True:
+        if datetime.now().strftime("%d-%m-%Y %H:%M:%S") == datetime.datetime.strptime(time_to_send, "%d-%m-%Y %H:%M:%S"):
+            break
+            
+    for i in range(len(json_data)):
+        print(json_data[i])
+        print("Message sacssesfully sent!")
+
+        for i in range(4):
+            response = requests.post(f"http://localhost:5000/:4444/:{satellite}/sendPacket?endnode=",
+                                    data = {"packet":json_data[i]})
+
+            if len(response.text) == telecomand:
+                print("got an ack")
+                write_response_tofile(str(json_data[i]),response.text)
                 break
+
+            time.sleep(3)
+    
+    reset_json()
+    
             
-        for i in range(len(json_data)):
-            print(json_data[i])
-            await websocket.send(json_data[i])
-            print("Message sacssesfully sent!")
-            for i in range(3):
-                ack = await asyncio.wait_for(websocket.recv(),timeout=5)
-                if "ack" == ack:
-                    print("got an ack")
-                    break
-            
-        
-
-asyncio.get_event_loop().run_until_complete(
-    send_packets('ws://localhost:3000'))
 
 
+def main():
+    while True:
+        packets, , = open_as_json(FILE_NAME)
+        if len(packets) >= 1:
+            send_packets()
 
 
+main()
